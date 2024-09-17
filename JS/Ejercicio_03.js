719af381ddacef19ac75e5be30493a06906a5787
Dulce Yadira Salvador Antonio <230493@utxicotepec.edu.mx>
2024-09-17 15:41:57 -0600

    Practica3
    ejercicio 3 tipos de datos String, ig, Int y Symbol

---
+++ JS/Ejercicio_03.js
@@ -0,0 +1,35 @@
+console.warn("kldmnjkandjkndjknwejkn");
+
+
+// const numero ="38"
+// const numero2 =38
+
+//console.log(typeof numero)
+//console.log(typeof numero2)
+
+//BigInt
+//const numeroGrande = (9956254542121545124543658436546456563)
+//console.log(typeof numeroGrande)
+
+//const numero =10
+//const numero2 = 20
+//console.log(numero +Number(numeroGrande))
+
+
+//const numero ="30"
+//const numero2 = 30
+
+//console.log(typeof String(numero2))
+//console.log(typeof Number(numero))
+
+//Symbol
+
+//const primerSymbol = Symbol(30)
+//const segundoSymbol = Symbol(30)
+
+//console.log(primerSymbol=== segundoSymbol)
+//console.log(primerSymbol.valueOf())
+
+///null
+//const descuento=null
+//console.log(typeof descuento)
